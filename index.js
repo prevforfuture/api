d3bc2f2494301177d6042b29ff4d7c78d3ad215d
erickson <ericksonicodemos@outlook.com>
2020-05-31 01:48:17 -0300

    alteração no index

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const server = require('http').createServer()
 const PORT = 80 || 8080
 
-const routes = require('./routes')
+const routes = require('./src/routes')
 
 
 app.use(express.json())
 app.use(routes)
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', express.static(path.join(__dirname, 'src')));
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}/`);
 });
