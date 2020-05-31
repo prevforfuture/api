0ea97a0a3ef6dbee7837a468eb038ef2cb42e771
erickson <ericksonicodemos@outlook.com>
2020-05-31 01:36:00 -0300

    alteração index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const server = require('http').createServer()
 const PORT = 80 || 8080
 
 const routes = require('./routes')
@@ -11,6 +12,6 @@ app.use(routes)
 
 app.use('/', express.static(path.join(__dirname, 'src')));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}/`);
 });
