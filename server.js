6222184df1c92d837d266f9ef290c88381d4b096
erickson <ericksonicodemos@outlook.com>
2020-05-31 02:29:13 -0300

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = 80 || 8080
+const PORT = process.env.PORT || 80
 
 const routes = require('./src/routes')
 
